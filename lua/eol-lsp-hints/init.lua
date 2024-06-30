b8e28a06342a87c838bce7cab780088b601ea618
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-06-30 16:54:43 +0200

    refactor: autoEnable logic

--- lua/eol-lsp-hints/init.lua
+++ lua/eol-lsp-hints/init.lua
@@ -2,39 +2,39 @@ local M = {}
 local inlayHintNs = vim.api.nvim_create_namespace("EolInlayHints")
 --------------------------------------------------------------------------------
 
-local function init()
-	require("eol-lsp-hints.override-handler")(inlayHintNs)
-	local config = require("eol-lsp-hints.config").config
+local function autoEnableHints()
+	-- initialize in already open buffers (in case of lazy-loading, etc.)
+	vim.iter(vim.lsp.get_clients())
+		:filter(function(client) return client.server_capabilities.inlayHintProvider end)
+		:each(function(client)
+			vim.iter(vim.lsp.get_buffers_by_client_id(client.id))
+				:each(function(bufnr) vim.lsp.inlay_hint.enable(true, { bufnr = bufnr }) end)
+		end)
 
-	-- lsp attach/detach
+	-- auto-enable/disable on lsp attach/detach
 	vim.api.nvim_create_autocmd({ "LspAttach", "LspDetach" }, {
 		group = vim.api.nvim_create_augroup("EolInlayHints", { clear = true }),
 		callback = function(ctx)
 			local client = vim.lsp.get_client_by_id(ctx.data.client_id)
 			local inlayHintProvider = client and client.server_capabilities.inlayHintProvider
-			if not inlayHintProvider or not config.autoEnableHints then return end
+			if not inlayHintProvider then return end
 
 			local enable = ctx.event == "LspAttach"
 			vim.lsp.inlay_hint.enable(enable, { bufnr = ctx.buf })
 			if not enable then vim.api.nvim_buf_clear_namespace(ctx.buf, inlayHintNs, 0, -1) end
 		end,
 	})
-
-	-- initialize in already open buffers (in case of lazy-loading, etc.)
-	if config.autoEnableHints then
-		for _, client in pairs(vim.lsp.get_clients()) do
-			local buffers = vim.lsp.get_buffers_by_client_id(client.id)
-			for _, bufnr in pairs(buffers) do
-				vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-			end
-		end
-	end
 end
 
----@param config? EolLspHints.config
-function M.setup(config)
-	require("eol-lsp-hints.config").setup(config)
-	init()
+---@param userConfig? EolLspHints.config
+function M.setup(userConfig)
+	require("eol-lsp-hints.config").setup(userConfig)
+	require("eol-lsp-hints.override-handler")(inlayHintNs)
+
+	local config = require("eol-lsp-hints.config").config
+	if config.autoEnableHints then
+		autoEnableHints()
+	end
 end
 
 --------------------------------------------------------------------------------
